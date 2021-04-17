9ad9b7254d851fe619572cec44d18bdfaa29089e
twsoft-ar <lvaccaro@twsoft.com.ar>
2021-04-17 00:10:26 -0300

    refactor

--- Source/TWS.RES.ReqManager/Server/TWS.RES.GeoITD.REQServer/REQService.cs
+++ Source/TWS.RES.ReqManager/Server/TWS.RES.GeoITD.REQServer/REQService.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.ServiceProcess;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using TWS.Networking;
 using TWS.RES.GeoCOM;
 
